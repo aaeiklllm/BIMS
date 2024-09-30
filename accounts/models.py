24e1a38aff9727120130056bae9fc3c055cc9916
Mikaella Louise Layug <92164973+aaeiklllm@users.noreply.github.com>
2024-09-30 16:20:09 +0800

    created models.py (database)

--- accounts/models.py
+++ accounts/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 class UserProfile(AbstractUser):
-    middle_name= models.CharField(max_length=1000, verbose_name='midde name', blank=True)
-    position = models.CharField(max_length=1000, verbose_name='position', blank=True)
-    unit = models.CharField(max_length=1000, verbose_name='unit', null=True, blank=True)
+    middle_name= models.CharField(max_length=150, verbose_name='midde name', blank=True)
+    position = models.CharField(max_length=150, verbose_name='position', blank=True)
+    unit = models.CharField(max_length=20, verbose_name='unit', null=True, blank=True)
     mobile_number = models.CharField(max_length=30, verbose_name='mobile number', blank=True)
     deletion_requested = models.BooleanField(default=False, verbose_name='Deletion Requested')
     deletion_approved = models.BooleanField(default=False, verbose_name='Deletion Approved')
 
 class Role(models.Model):
-    role= models.CharField(max_length=1000, default=None)
+    role= models.CharField(max_length=150, default=None)
 
 class UserroleMap(models.Model):
     user_id= models.ForeignKey(UserProfile, on_delete=models.CASCADE)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Samples(models.Model):
+    type = models.CharField(max_length=150, null=True, blank=False )
+    sex = models.CharField(max_length=1, null=True, blank=False)
+    age = models.IntegerField(null=True, blank=True)
+    clinical_diagnosis = models.CharField(max_length=150, null=True, blank=False )
+    amount = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=20, blank=True)
+    date_collected = models.DateField(null=True, blank=False) 
+    consent_form = models.FileField(blank=True, null=True)
+
+class Comorbidities(models.Model):
+    # Parent
+    sample_id = models.ForeignKey(Samples, on_delete=models.CASCADE)
+
+    comorbidity = models.CharField(max_length=150, blank=True)
+class Lab_Test(models.Model):
+    # Parent
+    sample_id = models.ForeignKey(Samples, on_delete=models.CASCADE)
+
+    labtest = models.CharField(max_length=150, blank=True)
+class Aliquot(models.Model):
+    # Parent
+    sample_id = models.ForeignKey(Samples, on_delete=models.CASCADE)
+
+    amount = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=20, blank=True)
+class Storage(models.Model):
+    # Parents
+    aliquot_id = models.ForeignKey(Aliquot, on_delete=models.CASCADE)
+    sample_id = models.ForeignKey(Samples, on_delete=models.CASCADE)
+
+    freezer_num = models.IntegerField(null=True, blank=True)
+    shelf_num = models.IntegerField(null=True, blank=True)
+    rack_num = models.IntegerField(null=True, blank=True)
+    box_num = models.IntegerField(null=True, blank=True)
+    container = models.CharField(max_length=20, blank=True)
+
+
