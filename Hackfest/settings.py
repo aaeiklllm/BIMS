78c7365e15faa0e6be60b9852f31332d038c6014
Giancarlo <gttrani@up.edu.ph>
2024-11-29 14:34:53 +0800

    frontend fixes

--- Hackfest/settings.py
+++ Hackfest/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bims',        # DB_DATABASE
         'USER': 'postgres',         # DB_USERNAME
-        'PASSWORD': 'antoninofamily', # DB_PASSWORD
+        'PASSWORD': '0143', # DB_PASSWORD
         'HOST': '127.0.0.1',        # DB_HOST
         'PORT': '5432',             # DB_PORT
     }
