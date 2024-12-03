4c426edba752998e3097525e501a7e0a481cda60
Mikaella Louise Layug <92164973+aaeiklllm@users.noreply.github.com>
2024-12-04 02:54:59 +0800

    deployment day 1 changes

--- Hackfest/settings.py
+++ Hackfest/settings.py
@@ -70,14 +70,26 @@ CSRF_TRUSTED_ORIGINS = ['https://bims-production.up.railway.app']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',  
+#         'USER': 'postgres', 
+#         'PASSWORD': 'NqdavVIIhAWmZrloOwcbaKVdITjydDCX', 
+#         'HOST': 'junction.proxy.rlwy.net',  
+#         'PORT': '12437', 
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',  
-        'USER': 'postgres', 
-        'PASSWORD': 'NqdavVIIhAWmZrloOwcbaKVdITjydDCX', 
-        'HOST': 'junction.proxy.rlwy.net',  
-        'PORT': '12437', 
+        'NAME': 'bims',        # DB_DATABASE
+        'USER': 'postgres',         # DB_USERNAME
+        'PASSWORD': 'mikabugsy', # DB_PASSWORD
+        'HOST': '127.0.0.1',        # DB_HOST
+        'PORT': '5432',             # DB_PORT
     }
 }
 
