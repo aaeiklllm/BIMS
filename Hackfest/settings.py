0d32f63a34b92e3405e62c48c5be20eb73347a0a
eeesantos <esantos@up.edu.ph>
2024-12-04 07:15:58 +0800

    comment changes

--- Hackfest/settings.py
+++ Hackfest/settings.py
@@ -73,11 +73,11 @@ CSRF_TRUSTED_ORIGINS = ['https://bims-production.up.railway.app']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',  
-        'USER': 'postgres', 
-        'PASSWORD': 'NqdavVIIhAWmZrloOwcbaKVdITjydDCX', 
-        'HOST': 'junction.proxy.rlwy.net',  
-        'PORT': '12437', 
+        'NAME': 'bims',        # DB_DATABASE
+        'USER': 'postgres',         # DB_USERNAME
+        'PASSWORD': 'admin123', # DB_PASSWORD
+        'HOST': '127.0.0.1',        # DB_HOST
+        'PORT': '5432',             # DB_PORT
     }
 }
 
